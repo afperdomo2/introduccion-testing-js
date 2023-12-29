a6657613b929722ed8209a480bc1882125bf1c9d
afperdomo2 <alvarado0925@gmail.com>
2023-12-29 17:02:06 -0500

    🧪api: Implementa pruebas de caja blanca con spies en el test de Books Service

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -11,16 +11,18 @@ const fakeBooks = [
   { _id: 4, name: 'The art of computer programming' },
 ];
 
+const mockGetAll = jest.fn();
+
 /**
  * Stub de MongoLib utilizado para pruebas.
  * @typedef {Object} MongoLibStub
  * @property {Function} getAll - Función que devuelve una copia de los libros falsos.
  * @property {Function} create - Función vacía utilizada para simular la creación de un libro.
  */
-const MongoLibStub = {
-  getAll: () => [...fakeBooks],
-  create: () => {},
-};
+// const MongoLibStub = {
+//   getAll: mockGetAll,
+//   create: () => {},
+// };
 
 /**
  * @description Mockear el módulo '../lib/mongo.lib' para simular su implementación.
@@ -32,7 +34,10 @@ const MongoLibStub = {
  * Esto permite simular el comportamiento de la librería de MongoDB y probar el servicio de libros
  * de forma aislada.
  */
-jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
+  create: () => {},
+})));
 
 describe('Test for Books Service', () => {
   let service;
@@ -45,17 +50,29 @@ describe('Test for Books Service', () => {
 
   describe('Test for getBooks method', () => {
     test('Should return an array of books', async () => {
+      // Arrange
+      mockGetAll.mockResolvedValue(fakeBooks);
       // Act
       const books = await service.getBooks({});
       console.log(books);
       // Assert
       expect(books.length).toEqual(4);
-      expect(books[0].name).toEqual('Clean Code');
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
     });
 
     test("should return a first book's name", async () => {
+      // Arrange
+      mockGetAll.mockResolvedValue([{ _id: 1, name: 'Rec' }]);
+      // Act
       const books = await service.getBooks({});
-      expect(books[0].name).toEqual('Clean Code');
+      console.log(books);
+      // Assert
+      expect(books[0].name).toEqual('Rec');
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
     });
   });
 });
